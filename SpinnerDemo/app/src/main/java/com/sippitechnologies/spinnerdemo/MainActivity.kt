65fbc79d19f8fa9b65e461a7ecdca7949de1ab3c
Sippitechnologies <sippitechnologies@gmail.com>
2020-04-05 22:30:09 +0530

    Spinner Implemation done

--- SpinnerDemo/app/src/main/java/com/sippitechnologies/spinnerdemo/MainActivity.kt
+++ SpinnerDemo/app/src/main/java/com/sippitechnologies/spinnerdemo/MainActivity.kt
@@ -8,28 +8,33 @@ import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.reflect.Array
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val countryOptions = arrayOf("Select Country","India","America","Pakisthan","England","Austriala")
-        val arrayAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,android.R.id.text1,countryOptions)
-        spinner.adapter = arrayAdapter
-        spinner.onItemSelectedListener = object : OnItemSelectedListener{
+        val countries = arrayOf<String>("India","USA","UK","Shri Lanka","Austriala","Pakistan","Bhutan")// Step 1 Take  data which you want to display in Spinner
+        val arrayAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,android.R.id.text1,countries)
+        //intialize adapter for Spinner
+        spinner_country.adapter=arrayAdapter // attach adapter with spinner
+        spinner_country.onItemSelectedListener = object:OnItemSelectedListener{ // we are attahing listener with spinner so that we dosomething on Spinner Item Selection
+
             override fun onItemSelected(
                 parent: AdapterView<*>?,
                 view: View?,
                 position: Int,
                 id: Long
             ) {
-                Toast.makeText(this@MainActivity,"Selected Item ${countryOptions[position]}",Toast.LENGTH_LONG).show()
+                textview_country.text=countries[position] // If we selection something from spinner we write our code here to perform action
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                Toast.makeText(this@MainActivity,"Nothign seleected",Toast.LENGTH_LONG).show()
+
             }
         }
+
+
     }
 }
