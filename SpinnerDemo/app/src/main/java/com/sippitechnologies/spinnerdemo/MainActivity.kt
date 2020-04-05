698da18156698e5192532134b92de8302079f17e
Sippitechnologies <sippitechnologies@gmail.com>
2020-04-05 18:16:57 +0530

    Implement Spinner

--- SpinnerDemo/app/src/main/java/com/sippitechnologies/spinnerdemo/MainActivity.kt
+++ SpinnerDemo/app/src/main/java/com/sippitechnologies/spinnerdemo/MainActivity.kt
@@ -2,11 +2,34 @@ package com.sippitechnologies.spinnerdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val countryOptions = arrayOf("Select Country","India","America","Pakisthan","England","Austriala")
+        val arrayAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,android.R.id.text1,countryOptions)
+        spinner.adapter = arrayAdapter
+        spinner.onItemSelectedListener = object : OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(this@MainActivity,"Selected Item ${countryOptions[position]}",Toast.LENGTH_LONG).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Toast.makeText(this@MainActivity,"Nothign seleected",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
