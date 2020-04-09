c8837f292a94174ed86f2073dd4ec3b94cebaede
Sippitechnologies <sippitechnologies@gmail.com>
2020-04-09 10:57:19 +0530

    Implement Toast

--- ToastDemo/app/src/main/java/com/sippitechnologies/toastdemo/MainActivity.kt
+++ ToastDemo/app/src/main/java/com/sippitechnologies/toastdemo/MainActivity.kt
@@ -2,11 +2,20 @@ package com.sippitechnologies.toastdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button5.setOnClickListener {
+            Toast.makeText(this,R.string.shortdurationtoast,Toast.LENGTH_SHORT).show()
+
+        }
+        button6.setOnClickListener {
+            Toast.makeText(this,R.string.toastforlongduration,Toast.LENGTH_LONG).show()
+        }
     }
 }
