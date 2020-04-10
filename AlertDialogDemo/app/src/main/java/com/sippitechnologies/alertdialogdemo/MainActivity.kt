0a036b5d9727b393a5461f3776060c8ae3d4a936
Sippitechnologies <sippitechnologies@gmail.com>
2020-04-10 09:41:00 +0530

    AlertDialog Implemented

--- AlertDialogDemo/app/src/main/java/com/sippitechnologies/alertdialogdemo/MainActivity.kt
+++ AlertDialogDemo/app/src/main/java/com/sippitechnologies/alertdialogdemo/MainActivity.kt
@@ -12,45 +12,50 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.setOnClickListener {
-            val alertDialogBuilder = AlertDialog.Builder(this);
-            alertDialogBuilder.setTitle("AlertDialog with One Button")
-                .setIcon(R.mipmap.ic_launcher)
-                .setMessage("I am Message for One Button Alert Dialog")
-                .setPositiveButton("Ok",DialogInterface.OnClickListener { dialog, which ->
-                    dialog.dismiss()
+        btn_one.setOnClickListener{
+            val builder= AlertDialog.Builder(this).setTitle(R.string.alertdailogwithonebutton)
+                .setMessage(R.string.alertdailogwithonebutton)
+                .setIcon(R.drawable.ic_launcher_background)
+                .setPositiveButton(R.string.submit,DialogInterface.OnClickListener { dialog, which ->
+                    Toast.makeText(this,R.string.submit_msg,Toast.LENGTH_LONG).show()
                 })
-           val alertdialog = alertDialogBuilder.create()
-            alertdialog.show()
-        }
-        button2.setOnClickListener{
-            val alertDialogBuilder = AlertDialog.Builder(this);
-            alertDialogBuilder.setTitle("AlertDialog with One Button")
-                .setIcon(R.mipmap.ic_launcher)
-                .setMessage("I am Message for One Button Alert Dialog")
-                .setPositiveButton("Ok", { dialog, which ->
-                    dialog.dismiss()
-                })
-            val alertdialog = alertDialogBuilder.create()
-            alertdialog.show()
+            val alertDialog= builder.create()
+            alertDialog.show()
+
 
         }
-        button3.setOnClickListener {
+        btn_two.setOnClickListener{
+            val builder= AlertDialog.Builder(this).setTitle(R.string.alertdialogwithtwbutton)
+                .setMessage(R.string.alertdialogwithtwbutton)
+                .setIcon(R.drawable.ic_launcher_background)
+                .setPositiveButton(R.string.submit,DialogInterface.OnClickListener { dialog, which ->
+                    Toast.makeText(this,R.string.submit_msg,Toast.LENGTH_LONG).show()
+                })
+                .setNegativeButton(R.string.cancel,DialogInterface.OnClickListener { dialog, which ->
+                    Toast.makeText(this,R.string.cancel_msg,Toast.LENGTH_LONG).show()
+                })
+            val alertDialog= builder.create()
+            alertDialog.show()
 
-            val alertDialogBuilder = AlertDialog.Builder(this);
-            alertDialogBuilder.setTitle("AlertDialog with One Button")
-                .setIcon(R.mipmap.ic_launcher)
-                .setMessage("I am Message for One Button Alert Dialog")
-                .setPositiveButton("Ok", { dialog, which ->
-                    dialog.dismiss()
-                })
-                .setNegativeButton("Cancel",DialogInterface.OnClickListener { dialog, which ->
-                    Toast.makeText(this,"Cancel is Clicked",Toast.LENGTH_LONG).show()
-                }).setNeutralButton("MayBe",{dialog,which->
-                    Toast.makeText(this,"May be Clicked",Toast.LENGTH_LONG).show()
-                })
-            val alertdialog = alertDialogBuilder.create()
-            alertdialog.show()
         }
+        btn_three.setOnClickListener{
+            val builder= AlertDialog.Builder(this).setTitle(R.string.alertdialogwithtwbutton)
+                .setMessage(R.string.alertdialogwithtwbutton)
+                .setIcon(R.drawable.ic_launcher_background)
+                .setPositiveButton(R.string.submit,DialogInterface.OnClickListener { dialog, which ->
+                    Toast.makeText(this,R.string.submit_msg,Toast.LENGTH_LONG).show()
+                })
+                .setNegativeButton(R.string.cancel,DialogInterface.OnClickListener { dialog, which ->
+                    Toast.makeText(this,R.string.cancel_msg,Toast.LENGTH_LONG).show()
+                })
+                .setNeutralButton(R.string.maybe,DialogInterface.OnClickListener { dialog, which ->
+                    Toast.makeText(this,R.string.maybe_msg,Toast.LENGTH_LONG).show()
+                })
+
+            val alertDialog= builder.create()
+            alertDialog.show()
+
+        }
+
     }
 }
