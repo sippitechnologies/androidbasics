cc32b8a8b6d7916ccd11157f37282f86a8fbc2be
Sippitechnologies <sippitechnologies@gmail.com>
2020-04-12 10:56:43 +0530

    Autocomplete implementation done

--- AutoCompleteTextView/app/src/main/java/com/sippitechnologies/autocompletetextview/MainActivity.kt
+++ AutoCompleteTextView/app/src/main/java/com/sippitechnologies/autocompletetextview/MainActivity.kt
@@ -13,5 +13,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val colorNames = arrayOf("Red","Green","Purple","Pink","Orange","Black","White","Brown","Aqua","Marron")
+        val colorAdapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,android.R.id.text1,colorNames)
+        autoCompleteTextView.setAdapter(colorAdapter)
+        autoCompleteTextView.setOnItemClickListener { parent, view, position, id ->
+
+            textView.text=autoCompleteTextView.text
+
+        }
     }
 }
