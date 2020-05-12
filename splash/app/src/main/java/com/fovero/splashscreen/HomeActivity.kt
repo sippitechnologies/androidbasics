d84e4b14b7957c699dd5a9a98dce0a59b0bfbd7a
Sippitechnologies <sippitechnologies@gmail.com>
2020-05-12 09:14:26 +0530

    implement Splash Screen

--- splash/app/src/main/java/com/fovero/splashscreen/HomeActivity.kt
+++ splash/app/src/main/java/com/fovero/splashscreen/HomeActivity.kt
@@ -1,15 +1,12 @@
-package com.sippitechnologies.recyclerview
+package com.fovero.splashscreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
-
-
 }
---
+++ splash/app/src/main/java/com/fovero/splashscreen/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.fovero.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        supportActionBar?.hide()
+        Handler().postDelayed(Runnable {
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+        },2000)
+
+    }
+}
