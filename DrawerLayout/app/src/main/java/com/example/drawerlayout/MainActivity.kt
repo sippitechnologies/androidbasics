a090b976780662862655a4947c87534cb4cac422
sippitechnologies <sippitechnologies@gmail.com>
2020-07-01 07:13:09 +0530

    Implemtation Done with Drawerlayout

--- DrawerLayout/app/src/main/java/com/example/drawerlayout/MainActivity.kt
+++ DrawerLayout/app/src/main/java/com/example/drawerlayout/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.MenuItem
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.GravityCompat
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -23,6 +27,12 @@ class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
         supportActionBar?.setDisplayShowHomeEnabled(true)
         supportFragmentManager.beginTransaction().add(R.id.container_root,HomeFragment()).commit()
         setUpActionBarToogle()
+        val header = navigationview_main.getHeaderView(0) as ConstraintLayout
+        val profileImageView = header.findViewById<ImageView>(R.id.imageView_profile)
+        val userEmail= header.findViewById<TextView>(R.id.textView_email);
+        profileImageView.setImageResource(R.drawable.ic_baseline_person_24)
+        userEmail.text = resources.getString(R.string.email)
+
 
     }
     fun setUpActionBarToogle()
