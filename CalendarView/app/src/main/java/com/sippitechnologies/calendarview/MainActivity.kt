ba67d5956a9ae3ad13ea72988592989c73e9a8bf
sippitechnologies <sippitechnologies@gmail.com>
2020-07-12 22:29:03 +0530

    Calendarview implemented

--- CalendarView/app/src/main/java/com/sippitechnologies/calendarview/MainActivity.kt
+++ CalendarView/app/src/main/java/com/sippitechnologies/calendarview/MainActivity.kt
@@ -2,10 +2,16 @@ package com.sippitechnologies.calendarview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        calendarView.setOnDateChangeListener { calendarView, i, i2, i3 ->
+            Toast.makeText(this@MainActivity,"Selected Date:$i3/$i2/$i",Toast.LENGTH_LONG).show()
+        }
+
     }
 }
