5019054ff7803ef36b9838e4f515fd186c89dce5
sippitechnologies <sippitechnologies@gmail.com>
2020-07-12 22:14:50 +0530

    Bottom Navigation Completed

--- BottomNavigationBar/app/src/main/java/com/sippitechnologies/bottomnavigationbar/MainActivity.kt
+++ BottomNavigationBar/app/src/main/java/com/sippitechnologies/bottomnavigationbar/MainActivity.kt
@@ -11,14 +11,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().add(R.id.root_cotainer,HomeFragment()).commit()
         bottomview.setOnNavigationItemSelectedListener {
 
-        val msg =    when(it.itemId)
+
+      /* Part One Logic
+       val msg =    when(it.itemId)
             {
                 R.id.about->"About Selected"
                     else-> "Home Selected"
             }
-            Toast.makeText(this@MainActivity,msg,Toast.LENGTH_LONG).show()
+            Toast.makeText(this@MainActivity,msg,Toast.LENGTH_LONG).show()*/
+
+            val fragment =    when(it.itemId)
+            {
+                R.id.about->AboutFragment()
+                else-> HomeFragment()
+            }
+            supportFragmentManager.beginTransaction().replace(R.id.root_cotainer,fragment).commit()
             true
         }
     }
