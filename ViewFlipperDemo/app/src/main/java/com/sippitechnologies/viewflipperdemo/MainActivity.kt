6f0545bb59598d09b4b387da84c058b64dae41d1
sippitechnologies <sippitechnologies@gmail.com>
2020-07-19 09:20:25 +0530

    Implement ImageSlider using View Flipper

--- ViewFlipperDemo/app/src/main/java/com/sippitechnologies/viewflipperdemo/MainActivity.kt
+++ ViewFlipperDemo/app/src/main/java/com/sippitechnologies/viewflipperdemo/MainActivity.kt
@@ -2,28 +2,57 @@ package com.sippitechnologies.viewflipperdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val imageView = ImageView(this)
-        imageView.setImageResource(R.drawable.coinimage_head)
-        imageView.scaleType=ImageView.ScaleType.FIT_XY
-        val imageView2 = ImageView(this)
-        imageView2.setImageResource(R.drawable.coin_tail)
-        imageView2.scaleType=ImageView.ScaleType.FIT_XY
+        val slides = arrayOf(Slide("C",R.drawable.c,"C is Great Lanaguage"),
+            Slide("Dot Net",R.drawable.dotnet,"Dot Net is Great Framework"),
+            Slide("Pascal",R.drawable.pascal,"Pascal is Great Lanaguage"),
+            Slide("Node Js",R.drawable.nodejs,"Node Js is Rock"),
+            Slide("Coin Tail",R.drawable.coin_tail,"Indian Coin Back Side")
+        ,Slide("Coin Head",R.drawable.coinimage_head,"Indian Coin Front Side"),
+                    Slide("Coin Image",R.drawable.coinimage,"Indian Coin both sides"))
 
-        viewflipper.addView(imageView,LinearLayout.LayoutParams(400,400))
-        viewflipper.addView(imageView2,LinearLayout.LayoutParams(400,400))
-        viewflipper.flipInterval=5000
-        viewflipper.setInAnimation(this,android.R.anim.slide_in_left)
-        viewflipper.setOutAnimation(this,android.R.anim.slide_out_right)
-        viewflipper.startFlipping()
+        slides.mapIndexed { index, slide ->
+            slider_root.addView(getSlideViewBySlideData(slide),index)
+        }
+       /* val inAmin =AnimationUtils.loadAnimation(this,android.R.anim.slide_in_left)
+        val outAnim =AnimationUtils.loadAnimation(this,android.R.anim.slide_out_right)*/
+        val inAmin =AnimationUtils.loadAnimation(this,android.R.anim.fade_in)
+        val outAnim =AnimationUtils.loadAnimation(this,android.R.anim.fade_out)
+        slider_root.inAnimation =inAmin
+        slider_root.outAnimation =outAnim
+        slider_root.flipInterval=2000
+        slider_root.startFlipping()
+
+
+
     }
+
+    fun getSlideViewBySlideData(slide:Slide):View
+    {
+        val view= LayoutInflater.from(this).inflate(R.layout.item_slide,null)
+        val title = view.findViewById<TextView>(R.id.textView_title)
+        val description = view.findViewById<TextView>(R.id.textView_description)
+        val logo = view.findViewById<ImageView>(R.id.image_logo)
+
+        title.text = slide.title
+        description.text = slide.description
+        logo.setImageResource(slide.logo)
+        return view
+
+    }
+
 }
---
+++ ViewFlipperDemo/app/src/main/java/com/sippitechnologies/viewflipperdemo/Slide.kt
@@ -0,0 +1,3 @@
+package com.sippitechnologies.viewflipperdemo
+
+data class Slide(val title:String,val logo:Int, val description:String)
