805ef5a754e151306500468b8277cff04de071dc
sippitechnologies <sippitechnologies@gmail.com>
2020-10-26 07:10:50 +0530

    Floating action implemented

--- floatingbutton/app/src/main/java/com/example/floatingbutton/MainActivity.kt
+++ floatingbutton/app/src/main/java/com/example/floatingbutton/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.floatingbutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        label.setOnClickListener {
+            if(floatingActionButton4.isVisible)
+            {
+                floatingActionButton4.hide()
+            }
+            else
+            {
+                floatingActionButton4.show()
+            }
+
+        }
+        floatingActionButton4.setOnClickListener {
+            floatingActionButton4.clearCustomSize()
+
+        }
+
+
     }
 }
